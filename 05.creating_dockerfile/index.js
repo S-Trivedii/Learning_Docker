c46a62590436f015089d3d5fea0678ddaa3ace11
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-11-05 18:40:21 +0530

    creating a docker file

---
+++ 05.creating_dockerfile/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 8001;
+
+app.get("/products", (req, res) => {
+  return res.json([
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
